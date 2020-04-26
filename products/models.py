bc758627472397fa491431e935b2011cb39f539c
unifiedsaad <saad.m@whyunified.com>
2020-04-27 00:37:54 +0500

    add product model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Prodcut(models.Model):
+    """
+    Product Model with following attributes
+    """
+    title = models.CharField(max_length=30)
+    description = models.TextField()
+
+    def __str__(self):
+        "Return string title representation of Product Object"
+        return self.title
